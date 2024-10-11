d4186128c970da3d169effc5b8aa7c20ecfb470b
SeyeongPark <lynn.yeong.102@gmail.com>
2024-10-12 00:39:26 +0900

    add playlist on listlist

--- src/components/LinkList.jsx
+++ src/components/LinkList.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Stack, Text, Input,
-  Box, Grid, Flex, Spacer,
+import {
+  Stack, Text, Input, Box, Button,
   Tabs, TabList, TabPanels, Tab, TabPanel,
-  Card, CardHeader, Heading, CardBody, CardFooter, Button, IconButton,
-  UnorderedList, ListItem } from '@chakra-ui/react';
-
-import { CloseIcon } from '@chakra-ui/icons'
+  Card, CardHeader, CardBody, UnorderedList,
+  ListItem, IconButton, CardFooter, Flex
+} from '@chakra-ui/react';
+import { CloseIcon } from '@chakra-ui/icons';
 
 function LinkList() {
   const defaultLinks = [
@@ -17,21 +17,21 @@ function LinkList() {
 
   const [links, setLinks] = useState([]);
   const [newLink, setNewLink] = useState('');
+  const [newIframe, setNewIframe] = useState('');
+  const [iframeSrc, setIframeSrc] = useState('');
 
   useEffect(() => {
-    // Load default links
     setLinks(defaultLinks);
-  
-    // Load additional links from localStorage
     const storedLinks = JSON.parse(localStorage.getItem('links'));
-    if (storedLinks) {
-      setLinks(storedLinks);
-    }
-  }, []);  
+    if (storedLinks) setLinks(storedLinks);
+    const storedIframe = localStorage.getItem('iframeSrc');
+    if (storedIframe) setIframeSrc(storedIframe);
+  }, []);
 
   useEffect(() => {
     localStorage.setItem('links', JSON.stringify(links));
-  }, [links]);
+    localStorage.setItem('iframeSrc', iframeSrc);
+  }, [links, iframeSrc]);
 
   const addLink = () => {
     if (newLink) {
@@ -45,7 +45,6 @@ function LinkList() {
     const updatedLinks = [...links];
     updatedLinks.splice(index, 1);
     setLinks(updatedLinks);
-    localStorage.setItem('links', JSON.stringify(updatedLinks));
   };
 
   const openMultipleLinks = () => {
@@ -54,72 +53,110 @@ function LinkList() {
     });
   };
 
-  // Function to normalize a link by adding "https://" if it's missing
   const normalizeLink = (link) => {
-    if (!link.startsWith("http://") && !link.startsWith("https://")) {
-      return "https://" + link;
+    return (link.startsWith("http://") || link.startsWith("https://")) ? link : "https://" + link;
+  };
+
+  const handleIframeChange = () => {
+    const regex = /<iframe[^>]+src="([^"]+)"/; // Adjusted regex to check for iframe
+    const match = regex.exec(newIframe);
+    
+    if (match && match[1]) {
+      const srcValue = match[1];
+      setIframeSrc(srcValue);
+      localStorage.setItem('iframeSrc', srcValue);
+    } else {
+      alert('Invalid iframe embed code. Please check the format.'); // Show warning
     }
-    return link;
+    
+    setNewIframe(''); // Clear input field
+  };  
+
+  const pasteFromClipboard = async () => {
+    try {
+      const text = await navigator.clipboard.readText();
+      setNewIframe(text);
+    } catch (err) {
+      console.error('Failed to read clipboard contents: ', err);
+    }
   };
 
   return (
     <>
-    <CardHeader fontSize='2xl'>
-      {/* Link List */}
-      <Tabs variant='soft-rounded' colorScheme='green'>
-        <TabList>
-          <Tab>Study</Tab>
-          <Tab>🎶</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-          <Box>
-            <Stack>
-              <Input 
-                size='lg'
-                type="text"
-                placeholder="Add a link"
-                value={newLink}
-                onChange={e => setNewLink(e.target.value)}
-              />
-              <Button onClick={addLink}>Add</Button>
-            </Stack>
-            <CardBody>
-            <UnorderedList>
-              {links.map((link, index) => (
-                <ListItem key={index}>
-                  <Text href={link} target="_blank" fontSize='md'>{link}
-                    <IconButton 
-                      onClick={() => deleteLink(index)} 
-                      colorScheme='tomato'
-                      color='tomato'
-                      marginLeft='20px'
-                      _hover={{ 
-                      bg: 'tomato',
-                      color: '#fff',
-                      transform: 'scale(0.98)',
-                      }}
-                      size='sm'
-                      variant='outline'
-                      aria-label='Delete'
-                      icon={<CloseIcon/>}/>
-                  </Text>
-                </ListItem>
-              ))}
-            </UnorderedList>
-          </CardBody>
-          <CardFooter justifyContent="flex-end">
-            <Button onClick={openMultipleLinks}>Open Links</Button>
-          </CardFooter>
-          </Box>
-            
-          </TabPanel>
-          <TabPanel>
-            COMING SOON
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </CardHeader>
+      <CardHeader fontSize='2xl'>
+        <Tabs variant='soft-rounded' colorScheme='green'>
+          <TabList>
+            <Tab>Study</Tab>
+            <Tab>🎶</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Box>
+                <Stack>
+                  <Input 
+                    size='lg'
+                    type="text"
+                    placeholder="Add a link"
+                    value={newLink}
+                    onChange={e => setNewLink(e.target.value)}
+                  />
+                  <Button onClick={addLink}>Add</Button>
+                </Stack>
+                <CardBody>
+                  <UnorderedList>
+                    {links.map((link, index) => (
+                      <ListItem key={index}>
+                        <Text href={link} target="_blank" fontSize='md'>{link}
+                          <IconButton 
+                            onClick={() => deleteLink(index)} 
+                            colorScheme='tomato'
+                            color='tomato'
+                            marginLeft='20px'
+                            size='sm'
+                            variant='outline'
+                            aria-label='Delete'
+                            icon={<CloseIcon/>}/>
+                        </Text>
+                      </ListItem>
+                    ))}
+                  </UnorderedList>
+                </CardBody>
+                <CardFooter justifyContent="flex-end">
+                  <Button onClick={openMultipleLinks}>Open Links</Button>
+                </CardFooter>
+              </Box>
+            </TabPanel>
+            <TabPanel>
+              <Flex align="center">
+                <Button onClick={pasteFromClipboard} marginRight="10px">Paste</Button>
+                <Input
+                  value={newIframe}
+                  onChange={(e) => setNewIframe(e.target.value)}
+                  placeholder="Insert iframe from Spotify"
+                  width="200px"
+                />
+                <Button onClick={handleIframeChange} marginLeft="10px" size="lg">DROP THE BEAT 📀</Button>
+              </Flex>
+              {iframeSrc && (
+                <Box marginTop="10px">
+                  <iframe
+                    style={{
+                      borderRadius: '12px',
+                      width: '100%',
+                      height: '352px',
+                    }}
+                    src={iframeSrc}
+                    frameBorder="0"
+                    allowFullScreen
+                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                    loading="lazy"
+                  ></iframe>
+                </Box>
+              )}
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </CardHeader>
     </>
   );
 }
